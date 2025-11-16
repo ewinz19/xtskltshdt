64c07f663d9a4c3b074f6c025602d3024eee373d
Born30 <47708077+ewinz19@users.noreply.github.com>
2025-11-16 15:17:40 +0800

    Create build_listch.sh

---
+++ data/sites/visionplus.id/build_listch.sh
@@ -0,0 +1,6 @@
+
+# Versi bahasa Indonesia
+node -e "require('./visionplus.id.config.js').saveChannelsXML('./visionplus.id_id.channels.xml', 'id')"
+
+# Versi bahasa English
+node -e "require('./visionplus.id.config.js').saveChannelsXML('./visionplus.id_en.channels.xml', 'en')"
