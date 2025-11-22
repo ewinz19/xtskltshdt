15f8a8ac74c46316d774b1eb100cada6115e94a4
cahaya-malam <winz44winz@gmail.com>
2025-11-22 18:45:18 +0800

    Delete data directory Data

--- data/sites/dens.tv/build_listch.sh
+++
@@ -1,1 +0,0 @@
-node -e "require('./dens.tv.config.js').saveChannelsXML('./dens.tv_id.channels.xml')"
--- data/sites/visionplus.id/build_epg.sh
+++
@@ -1,1 +0,0 @@
-npm run grab --- --channels=/data/data/com.termux/files/home/Tool/epg/sites/visionplus.id/visionplus.id_id.channels.xml
